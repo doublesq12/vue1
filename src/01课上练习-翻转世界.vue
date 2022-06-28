9c558b331ef13aeeec43ed9cce5d4fedb04e49b8
doublekou <951513186@qq.com>
2022-06-28 19:49:43 +0800

    课上练习-翻转世界

---
+++ src/01课上练习-翻转世界.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>{{msg}}</h1>
+      <button @click="clickFn">翻转</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg:'Hello, Vue!'
+    };
+  },
+  methods: {
+    clickFn () {
+      this.msg = this.msg.split('').reverse().join('');
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
