d46a8b9ad486ef4502f7382456a1353eac001858
doublekou <951513186@qq.com>
2022-06-28 19:48:01 +0800

    课上练习-翻转世界

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <h1>{{msg}}</h1>
+      <button @click="clickFn">翻转</button>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      msg:'Hello, Vue!'
+    };
+  },
+  methods: {
+    clickFn () {
+      this.msg = this.msg.split('').reverse().join('');
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
