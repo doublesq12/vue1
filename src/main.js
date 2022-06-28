3e3abf692d127d41a80089804ee58beaafe4435b
doublekou <951513186@qq.com>
2022-06-28 20:41:36 +0800

    计算字符串中每个字符出现的次数

--- src/main.js
+++ src/main.js
@@ -6,3 +6,16 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+let str='asdasdasdasdasdasd'
+let names = str.split('')
+let countedNames = names.reduce(function (allNames, name) { 
+  if (name in allNames) {
+    allNames[name] ++
+  }
+  else {
+    allNames[name]=1
+  }
+  return allNames
+},{})
+console.log(countedNames);
